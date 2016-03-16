a2066f78a0bc688cf6211d348e1efc8377a68ddc
Rob Berger <rob.a.berger@gmail.com>
2016-03-16 18:16:28 -0400

    start work on cbcalc function and styling

--- allcards/config/routes.rb
+++ allcards/config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   root 'credit_cards#index'
 
   resources :credit_cards
+
+  post 'credit_cards/cbcalc'
 
   resources :signup_rewards
 
