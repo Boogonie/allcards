03611f326c52d8e758bd0641c8196872c4792ad4
Rob Berger <rob.a.berger@gmail.com>
2016-03-21 12:29:29 -0400

    add styling to Cash Back Calculator Results

--- allcards/app/controllers/calculators_controller.rb
+++ allcards/app/controllers/calculators_controller.rb
@@ -1,22 +1,22 @@
 class CalculatorsController < ApplicationController
 
   def cbcalc
-    # @category_rate = { :Groceries => params[:groceries],
-    #                    :Gas       => params[:gas],
-    #                    :Everyday  => params[:everyday]
-    #                  }
+    @category_rate = { :Groceries => params[:groceries],
+                       :Gas       => params[:gas],
+                       :Everyday  => params[:everyday]
+                     }
 
     #groceries = params[:groceries]
 
     @credit_cards = CreditCard.all.includes([:categories, :reward_rates])
 
-    @credit_cards.each do |card|
-      #category_rate.each do
-        reward_rate = card.reward_rates.find{ |rr| rr.category.name == 'Groceries' }
-        puts "#{reward_rate}"
-        @cash_back  = reward_rate.percent * params[:groceries].to_i / 100
-      #end
-    end
+    # @credit_cards.each do |card|
+    #   #category_rate.each do
+    #     reward_rate = card.reward_rates.find{ |rr| rr.category.name == 'Groceries' }
+    #     puts "#{reward_rate}"
+    #     @cash_back  = reward_rate.percent * params[:groceries].to_i / 100
+    #   #end
+    # end
   end
 
 end
