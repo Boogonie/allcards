caa56e3a709cee9e9b546463ea313ee40bf36548
Rob Berger <rob.a.berger@gmail.com>
2016-03-22 17:08:13 -0400

    move cbcalc view code to the controller

--- allcards/app/controllers/calculators_controller.rb
+++ allcards/app/controllers/calculators_controller.rb
@@ -5,6 +5,16 @@ class CalculatorsController < ApplicationController
 
     @credit_cards = CreditCard.all #.includes([:categories, :reward_rates])
 
+    @credit_cards.each do |card|
+      
+      @cash_back = ((card.reward_rate_by_category('Groceries')         * params[:groceries].to_i) +
+                    (card.reward_rate_by_category('Gas')               * params[:gas].to_i)       +
+                    (card.reward_rate_by_category('Everyday Spending') * params[:everyday].to_i)) /
+                    100 * 12
+
+      @reward_rate = @cash_back / ((params[:groceries].to_i + params[:gas].to_i + params[:everyday].to_i) * 12) * 100
+    end
+
   end
 
   def cbcompare
