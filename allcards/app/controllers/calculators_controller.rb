797a9d4c3373d1d86c8080d372f175d759a726fc
Rob Berger <rob.a.berger@gmail.com>
2016-03-22 09:00:17 -0400

    bug fixes and styling

--- allcards/app/controllers/calculators_controller.rb
+++ allcards/app/controllers/calculators_controller.rb
@@ -10,6 +10,21 @@ class CalculatorsController < ApplicationController
 
     @credit_cards = CreditCard.all.includes([:categories, :reward_rates])
 
+    @credit_cards.each do |card|
+
+      reward_rate_groceries = card.reward_rates.find{ |rr| rr.category.name == 'Groceries' }
+      reward_rate_gas = card.reward_rates.find{ |rr| rr.category.name == 'Gas' }
+      reward_rate_everyday = card.reward_rates.find{ |rr| rr.category.name == 'Everyday Spending' }
+
+      @cash_back  =  ((reward_rate_groceries.percent * params[:groceries].to_i) +
+                      (reward_rate_gas.percent       * params[:gas].to_i)       +
+                      (reward_rate_everyday.percent  * params[:everyday].to_i))  /
+                      100 * 12
+      @reward_rate = @cash_back / ((params[:groceries].to_i + params[:gas].to_i + params[:everyday].to_i) * 12) * 100
+
+    end
+
+
     # @credit_cards.each do |card|
     #   #category_rate.each do
     #     reward_rate = card.reward_rates.find{ |rr| rr.category.name == 'Groceries' }
