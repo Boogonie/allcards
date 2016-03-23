88dfe0f20bf2608aab04d0253b225dca6de9d1ae
Rob Berger <rob.a.berger@gmail.com>
2016-03-22 20:09:56 -0400

    add bug fixes and jquery for calculators

--- allcards/app/controllers/calculators_controller.rb
+++ allcards/app/controllers/calculators_controller.rb
@@ -3,7 +3,7 @@ class CalculatorsController < ApplicationController
 
   def cbcalc
 
-    @credit_cards = CreditCard.all #.includes([:categories, :reward_rates])
+    @credit_cards = CreditCard.all
 
     @credit_cards_and_rewards = @credit_cards.inject({}) do |memo, card|
       cash_back = ( (card.monthly_reward_by_category('Groceries'        , params[:groceries].to_i)) +
@@ -23,11 +23,22 @@ class CalculatorsController < ApplicationController
     @credit_cards = CreditCard.all
 
     @my_cash_back = ((params[:my_groceries].to_i * params[:groceries].to_i) +
-                    (params[:my_gas].to_i       * params[:gas].to_i)       +
-                    (params[:my_everyday].to_i  * params[:everyday].to_i)) /
-                    100 * 12
+                     (params[:my_gas].to_i       * params[:gas].to_i)       +
+                     (params[:my_everyday].to_i  * params[:everyday].to_i)) /
+                     100 * 12
 
     @my_reward_rate = @my_cash_back.to_f / ((params[:groceries].to_i + params[:gas].to_i + params[:everyday].to_i) * 12) * 100
+
+    @credit_cards_and_rewards = @credit_cards.inject({}) do |memo, card|
+      cash_back =  ((card.monthly_reward_by_category('Groceries'        , params[:groceries].to_i)) +
+                    (card.monthly_reward_by_category('Gas'              , params[:gas].to_i))       +
+                    (card.monthly_reward_by_category('Everyday Spending', params[:everyday].to_i))) /
+                    100 * 12
+
+      reward_rate = cash_back / ((params[:groceries].to_i + params[:gas].to_i + params[:everyday].to_i) * 12) * 100
+
+      memo.merge(card => { cash_back: cash_back, reward_rate: reward_rate })
+    end
 
   end
 
